19850206a4b08f529e256124b9d60494c6cb9d25
noadev <67566122+thenoadev@users.noreply.github.com>
2023-02-12 22:01:33 -0500

    timezone actually works this time (hopefully)

--- api/weatherapi.js
+++ api/weatherapi.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 var SunCalc = require('suncalc');
+const moment = require('moment-timezone');
 
 exports.handler = async function (event, context) {
   let openweather;
@@ -7,7 +8,7 @@ exports.handler = async function (event, context) {
   try {
     openweather = await fetch('https://api.openweathermap.org/data/2.5/weather?id=6077246&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece');
     const openweather_res = await openweather.json();
-    var times = SunCalc.getTimes(new Date(new Intl.DateTimeFormat(['en-US'], {timeZone: 'America/New_York', timeZoneName: 'short'}).format(new Date())), 45.5001, -73.6825);
+    var times = SunCalc.getTimes(moment().tz('America/New_York'), 45.5001, -73.6825);
     var sunriseStr = times.sunrise.getHours() + ':' + (times.sunrise.getMinutes() < 10 ? '0' + times.sunrise.getMinutes() : times.sunrise.getMinutes());
     var sunsetStr = times.sunset.getHours() + ':' + (times.sunset.getMinutes() < 10 ? '0' + times.sunset.getMinutes() : times.sunset.getMinutes())
     return {
