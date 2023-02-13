3e1bfe6938702099a1775c61fe71aa5fa05f190c
noadev <67566122+thenoadev@users.noreply.github.com>
2023-02-12 21:26:28 -0500

    initialized variable

--- api/weatherapi.js
+++ api/weatherapi.js
@@ -1,10 +1,12 @@
 const fetch = require("node-fetch");
 
 exports.handler=async function(event, context) {
+  let openweather;
+  let sun;
   try {
-    const openweather = await fetch('https://api.openweathermap.org/data/2.5/weather?id=6077246&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece');
+    openweather = await fetch('https://api.openweathermap.org/data/2.5/weather?id=6077246&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece');
     const openweather_res = await openweather.json();
-    const sun = await fetch('https://api.sunrisesunset.io/json?lat=45.5001&lng=-73.6825&timezone=EST&date=today');
+    sun = await fetch('https://api.sunrisesunset.io/json?lat=45.5001&lng=-73.6825&timezone=EST&date=today');
     const sun_res = await sun.json();
     return {
       statusCode: 200,
